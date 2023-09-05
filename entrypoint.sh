19db3b91a317ac268534356f44e905f77d946647
Andy Oknen <andy.oknen@yandex.ru>
2023-09-05 15:47:00 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,9 @@ if ! [[ $TAG = "" ]] ; then
     tagarg="-t ${TAG}"
 fi
 
+echo $GITHUB_WORKSPACE
+ls -l $GITHUB_WORKSPACE/
+
 mkdir $GITHUB_WORKSPACE/out
 ln -s $GITHUB_WORKSPACE/out /out
 /build -p $PLATFORM -b $BRANCH -d $DEPSBASE $tagarg
