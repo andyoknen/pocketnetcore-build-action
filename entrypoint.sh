472087b59564779448acff017c73e7c40896eb16
Andy Oknen <andy.oknen@yandex.ru>
2024-10-30 21:52:01 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,14 +3,13 @@ set -o errexit
 
 [ -n $PLATFORM ] || fail "No PLATFORM was supplied."
 [ -n $BRANCH ] || fail "No BRANCH was supplied."
-[ -n $DEPSBASE ] || fail "No DEPSBASE was supplied."
 
 tagarg=""
 if ! [[ $TAG = "" ]] ; then
     tagarg="-t ${TAG}"
 fi
 
-/build -p $PLATFORM -b $BRANCH -d $DEPSBASE $tagarg
+/build -p $PLATFORM -b $BRANCH $tagarg
 
 mkdir $GITHUB_WORKSPACE/out
 cp /out/* $GITHUB_WORKSPACE/out/
